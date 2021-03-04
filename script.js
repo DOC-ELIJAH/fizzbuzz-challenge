d046ea7c98bef461dfb4ad3ab44455ebf99534d0
Hope Elijah <hopelijah98@gmail.com>
2021-03-04 16:29:44 +0100

    fizzbuzz challenge

---
+++ script.js
@@ -0,0 +1,16 @@
+let fizzChallenge = () => {
+  for (i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log("fizzbuzz");
+    }
+    if (i % 3 === 0) {
+      console.log("fizz");
+    }
+    if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+};
+fizzChallenge();
